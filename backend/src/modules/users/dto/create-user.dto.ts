c28007cfba21d40f826cda1babb2a0768c5ca3e9
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-11-02 17:06:07 -0300

    chore: create user dto and entity

--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'Patrick' })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: 'patrick@mail.com' })
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description:
+      'O número de telefone deve ser um número válido no formato brasileiro.',
+    example: '(14)991335454',
+  })
+  @IsPhoneNumber('BR')
+  @IsString()
+  phone: string;
+}
--- backend/src/modules/users/entities/user.entity.ts
+++ backend/src/modules/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+
+export class User {
+  readonly id: string;
+  name: string;
+  email: string;
+  phone: string;
+
+  @Exclude()
+  readonly createdAt: Date;
+
+  @Exclude()
+  readonly updatedAt: Date;
+
+  @Exclude()
+  readonly deletedAt: Date | null;
+
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+    this.deletedAt = null;
+  }
+}
