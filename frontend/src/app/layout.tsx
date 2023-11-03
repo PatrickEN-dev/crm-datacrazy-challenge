613acbbd7e050409a0196e227f3d755b1f588771
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-11-03 17:10:49 -0300

    fix: remove ModalProvider from provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,12 @@
 import "./globals.css";
 import { UsersProvider } from "@/contexts/Users/users.context";
 import { IChildrenProps } from "@/@types/context.global";
-import { ModalProvider } from "@/contexts/Modal/modal.context";
 
 export default function RootLayout({ children }: IChildrenProps) {
   return (
     <html lang="en">
       <body>
-        <UsersProvider>
-          <ModalProvider>{children}</ModalProvider>
-        </UsersProvider>
+        <UsersProvider>{children}</UsersProvider>
       </body>
     </html>
   );
