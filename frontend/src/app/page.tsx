fffe1409a8bb5e381535caae952185b9f1de6286
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-11-05 23:51:33 -0300

    chore: delete testes to backend and rename headerComponent

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import CreateUserButton from "@/components/CreateUserButton/createUserButton";
 import UserCardList from "@/components/card/UserCardList/userCardList";
-import NewHeader from "@/components/newHeader/newHeader";
+import NewHeader from "@/components/Header/header";
 
 export default function Home() {
   return (
     <div>
       <NewHeader />
 
-      <main className="flex flex-col items-center h-full w-full">
+      <main className="flex flex-col items-center h-full w-full dark:bg-slate-600">
         <div className="h-full mt-5">
           <CreateUserButton />
         </div>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -16,6 +16,7 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    darkMode: "class",
     extend: {
       keyframes: {
         "accordion-down": {
